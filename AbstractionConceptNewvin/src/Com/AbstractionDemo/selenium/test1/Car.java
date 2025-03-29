9b45150db13a79ca465c76a8c1bda970f12aa33e
Administrator <Administrator@Vinesh-PC>
2025-03-29 16:25:06 +0530

    vintest

--- AbstractionConceptNewvin/src/Com/AbstractionDemo/selenium/test1/Car.java
+++ AbstractionConceptNewvin/src/Com/AbstractionDemo/selenium/test1/Car.java
@@ -2,6 +2,7 @@ package Com.AbstractionDemo.selenium.test1;
 
 public interface Car {
 	//Always define only abstract methods
+	//delete this just trying to push GITHUB
 	//no method body
 	//only method declaration
 	//We achieve 100% abstraction
